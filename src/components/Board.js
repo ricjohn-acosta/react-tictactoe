323d76effdb7f4319507d0ced9ca7e3a536173ae
Ricjohn Acosta <41725332+ricjohn-acosta@users.noreply.github.com>
2020-01-27 18:31:36 +1300

    write display board

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Board() {
+  return (
+    <div className="grid-container">
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  )
+}
+
+export default Board;
