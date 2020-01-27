204e822a6440bac7b100e3a37f9fddf458572c0d
Ricjohn Acosta <41725332+ricjohn-acosta@users.noreply.github.com>
2020-01-27 18:55:05 +1300

    make buttons responsive

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 function Board() {
+  function sayHello(e) {
+    console.log('hello')
+    console.log(e.target.id)
+  }
   return (
     <div className="grid-container">
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
+      <button id="1" onClick={sayHello}></button>
+      <button id="2" onClick={sayHello}></button>
+      <button id="3" onClick={sayHello}></button>
+      <button id="4" onClick={sayHello}></button>
+      <button id="5" onClick={sayHello}></button>
+      <button id="6" onClick={sayHello}></button>
+      <button id="7" onClick={sayHello}></button>
+      <button id="8" onClick={sayHello}></button>
+      <button id="9" onClick={sayHello}></button>
     </div>
   )
 }
